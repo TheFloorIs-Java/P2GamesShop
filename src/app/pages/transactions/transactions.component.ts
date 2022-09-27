5e9e515e3e8ed5ce5dab203dacb8ac4a775f56c3
Jamie <jnguyen127@horizon.csueastbay.edu>
2022-09-27 10:36:27 -0700

    view all button shows/hides a div when clicked

--- src/app/pages/transactions/transactions.component.ts
+++ src/app/pages/transactions/transactions.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transactions.component.css']
 })
 export class TransactionsComponent implements OnInit {
+  viewAllButtonPressed : boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onViewAllButtonClicked() : void {
+    this.viewAllButtonPressed = !this.viewAllButtonPressed;
+  }
 }
