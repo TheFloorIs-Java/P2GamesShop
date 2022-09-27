ddfd211682c0c9310c05b4c6feb34207cca05292
Jamie <jnguyen127@horizon.csueastbay.edu>
2022-09-27 10:21:08 -0700

    set up pages directory, no functionality button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TransactionsComponent } from './pages/transactions/transactions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"transactions", component:TransactionsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TransactionsComponent } from './pages/transactions/transactions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransactionsComponent
   ],
   imports: [
     BrowserModule,
