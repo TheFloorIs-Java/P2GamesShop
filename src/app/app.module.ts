01340cbdb285b97ee7d565be69232fd31f02f7e2
REuen84 <rileyeuen84@gmail.com>
2022-09-30 16:36:15 -0400

    imported some things

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AllTransactionsComponent } from './pages/all-transactions/all-transactions.component';
@@ -22,7 +22,8 @@ import { AccountManagementComponent } from './pages/admin/account-management/acc
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
