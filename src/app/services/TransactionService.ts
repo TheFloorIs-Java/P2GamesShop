20b9878465c2f08de706d288f2e7b0eb2e2240b3
Jamie <jnguyen127@horizon.csueastbay.edu>
2022-10-05 11:35:52 -0700

    hooked up getAllTransactions() to a real endpoint

--- src/app/services/TransactionService.ts
+++ src/app/services/TransactionService.ts
@@ -16,11 +16,14 @@ export class TransactionService {
         return this.http.get<Array<Transaction>>("https://633335f7433198e79dc183b0.mockapi.io/transactions");
     }
 
-    // getAllTransactionsFromUser
+    // Get all transactions from specified user
     getAllTransactionsFromUser(user_id:number) : Observable<Array<Transaction>> {
         // https://p2gamesstore.azurewebsites.net/{username/user_id}/transactions
         return this.http.get<Array<Transaction>>("https://p2gamesstore.azurewebsites.net/" + user_id +"/transactions");
     }
 
-
+    // Get all transaction on specified date
+    getAllTransactionsByDate(date:Date) : Observable<Array<Transaction>> {
+        return this.http.get<Array<Transaction>>("https://p2gamesstore.azurewebsites.net/transactions/" + date);
+    }
 }
